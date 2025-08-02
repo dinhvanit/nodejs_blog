ec7495240a8cd3d15a40712a6755963bed0a4b77
dinhvanit <nguyendinhvanefto@gmail.com>
2025-08-02 16:44:03 +0700

    Restructure codebase using MVC pattern

---
+++ src/app/controllers/NewsControllers.js
@@ -0,0 +1,15 @@
+class NewsController {
+
+    // [GET] /news
+    index(req, res) {
+        res.render("news");
+    }
+
+    // [GET] /news/:slug
+    show(req, res) {
+        res.send("News detail page for slug: " + req.params.slug);   
+    }
+}
+
+
+module.exports = new NewsController;
--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ const handlebars = require("express-handlebars");
 const app = express();
 const port = 3000;
 
+
+const route = require("./routes/index");
+
 app.use(express.static(path.join(__dirname, "public")));
 
 // HTTP logger
@@ -20,14 +23,7 @@ app.engine(
 app.set("view engine", "hbs");
 app.set("views", path.join(__dirname, "resources/views"));
 
-// Routes
-app.get("/", (req, res) => {
-  res.render("home");
-});
-
-app.get("/news", (req, res) => {
-  res.render("home");
-});
+route(app);
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
